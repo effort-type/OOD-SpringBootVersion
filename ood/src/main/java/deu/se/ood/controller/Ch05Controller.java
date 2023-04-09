119396c5409352edd9f6298d43ffb56bc7fbe7bb
Minsu <93813747+effort-type@users.noreply.github.com>
2023-04-09 19:19:24 +0900

    Feat#10 (#12)
    * feat : tagfile 생성 및 구현 (#10)
    
    * feat : tagfile View 구현 (#10)
    
    * feat : tagFile controller 구현 (#10)

--- ood/src/main/java/deu/se/ood/controller/Ch05Controller.java
+++ ood/src/main/java/deu/se/ood/controller/Ch05Controller.java
@@ -4,15 +4,18 @@
  */
 package deu.se.ood.controller;
 
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  *
- * @author admin
+ * @author 정민수
  */
 @Controller
+@Slf4j
 public class Ch05Controller {
     
     @GetMapping("/ch05/eltest")
@@ -34,4 +37,18 @@ public class Ch05Controller {
         return String.format("ch05/simpletagtest/index%d", number);
     }
     
+    /**
+     * 
+     * @param number 1 또는 2만 허용 (programming by contract에 의해 별도의 유효성 테스트는 하지 않음)
+     * @return 
+     */
+    @GetMapping({"/ch05/tagfiletest", "/ch05/tagfiletest/{number}"})
+    public String tagFileTest(@PathVariable Optional<Integer> number) {
+        String execUrl = "ch05/tagfiletest/index";
+        if (number.isPresent()) { // number 값이 있으면 그대로 사용, 없으면 디폴트로 설정되어 있는 execUrl 값 사용
+            execUrl += String.format("%d", number.get());
+        }
+        log.debug("execUrl = {}", execUrl);
+        return execUrl;
+    }    
 }
